cc97ff440545781e4586006af8eeca8c3818936c
chun.yin.to <tobytoyin1234@gmail.com>
2023-09-21 13:13:32 +0100

    add table factory

--- crossfilter_dataframe/tables/__init__.py
+++ crossfilter_dataframe/tables/__init__.py
@@ -1,2 +1,11 @@
+from ..types import Data
 from .base import Table
 from .pandas_dataframe import PandasTable
+
+
+def table_factory(table_type: str, data: Data) -> Table:
+    _map = {
+        'pandas': PandasTable
+    }
+    
+    return _map.get(table_type)(data)
